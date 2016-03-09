a8a6ab235b719b0648d5e00aaa31caaf7cd8567b
Nuss Maciej <informatyk@nuss.pl>
2016-03-09 15:37:10 +0100

    Uruchomiony algorytm szyfrujący !

--- Szyfrator/Szyfrator/Szyfrator.Designer.cs
+++ Szyfrator/Szyfrator/Szyfrator.Designer.cs
@@ -32,52 +32,74 @@
             this.txtout = new System.Windows.Forms.TextBox();
             this.btnKoduj = new System.Windows.Forms.Button();
             this.btnDekoduj = new System.Windows.Forms.Button();
+            this.klucz = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtin
             // 
-            this.txtin.Location = new System.Drawing.Point(12, 12);
+            this.txtin.Location = new System.Drawing.Point(12, 63);
             this.txtin.Name = "txtin";
             this.txtin.Size = new System.Drawing.Size(341, 20);
             this.txtin.TabIndex = 0;
             // 
             // txtout
             // 
-            this.txtout.Location = new System.Drawing.Point(12, 91);
+            this.txtout.Location = new System.Drawing.Point(11, 118);
             this.txtout.Name = "txtout";
             this.txtout.Size = new System.Drawing.Size(341, 20);
             this.txtout.TabIndex = 1;
-            this.txtout.TextChanged += new System.EventHandler(this.txtout_TextChanged);
             // 
             // btnKoduj
             // 
-            this.btnKoduj.Location = new System.Drawing.Point(12, 49);
+            this.btnKoduj.Location = new System.Drawing.Point(12, 89);
             this.btnKoduj.Name = "btnKoduj";
             this.btnKoduj.Size = new System.Drawing.Size(126, 23);
             this.btnKoduj.TabIndex = 2;
             this.btnKoduj.Text = "Koduj";
             this.btnKoduj.UseVisualStyleBackColor = true;
+            this.btnKoduj.Click += new System.EventHandler(this.btnKoduj_Click);
             // 
             // btnDekoduj
             // 
-            this.btnDekoduj.Location = new System.Drawing.Point(227, 49);
+            this.btnDekoduj.Location = new System.Drawing.Point(227, 89);
             this.btnDekoduj.Name = "btnDekoduj";
             this.btnDekoduj.Size = new System.Drawing.Size(126, 23);
             this.btnDekoduj.TabIndex = 3;
             this.btnDekoduj.Text = "Dekoduj";
             this.btnDekoduj.UseVisualStyleBackColor = true;
+            this.btnDekoduj.Click += new System.EventHandler(this.btnDekoduj_Click);
+            // 
+            // klucz
+            // 
+            this.klucz.Location = new System.Drawing.Point(12, 26);
+            this.klucz.Name = "klucz";
+            this.klucz.Size = new System.Drawing.Size(340, 20);
+            this.klucz.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Klucz szyfrujący";
             // 
             // Szyfrator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(365, 129);
+            this.ClientSize = new System.Drawing.Size(365, 148);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.klucz);
             this.Controls.Add(this.btnDekoduj);
             this.Controls.Add(this.btnKoduj);
             this.Controls.Add(this.txtout);
             this.Controls.Add(this.txtin);
             this.Name = "Szyfrator";
             this.Text = "Szyfrator";
+            this.Load += new System.EventHandler(this.Szyfrator_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +111,8 @@
         private System.Windows.Forms.TextBox txtout;
         private System.Windows.Forms.Button btnKoduj;
         private System.Windows.Forms.Button btnDekoduj;
+        private System.Windows.Forms.TextBox klucz;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Szyfrator/Szyfrator/Szyfrator.cs
+++ Szyfrator/Szyfrator/Szyfrator.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+
 
 namespace Szyfrator
 {
@@ -16,10 +18,69 @@ namespace Szyfrator
         {
             InitializeComponent();
         }
+        public static string Encrypt(string strToEncrypt, string strKey)
+        {
+            try
+            {
+                TripleDESCryptoServiceProvider objDESCrypto = new TripleDESCryptoServiceProvider();
+                MD5CryptoServiceProvider objHashMD5 = new MD5CryptoServiceProvider();
 
-        private void txtout_TextChanged(object sender, EventArgs e)
+                byte[] byteHash, byteBuff;
+                string strTempKey = strKey;
+
+                byteHash = objHashMD5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(strTempKey));
+                objHashMD5 = null;
+                objDESCrypto.Key = byteHash;
+                objDESCrypto.Mode = CipherMode.ECB; //CBC, CFB
+
+                byteBuff = ASCIIEncoding.ASCII.GetBytes(strToEncrypt);
+                return Convert.ToBase64String(objDESCrypto.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            }
+            catch (Exception ex)
+            {
+                return "Wrong Input. " + ex.Message;
+            }
+        }
+
+        public static string Decrypt(string strEncrypted, string strKey)
+        {
+            try
+            {
+                TripleDESCryptoServiceProvider objDESCrypto = new TripleDESCryptoServiceProvider();
+                MD5CryptoServiceProvider objHashMD5 = new MD5CryptoServiceProvider();
+
+                byte[] byteHash, byteBuff;
+                string strTempKey = strKey;
+
+                byteHash = objHashMD5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(strTempKey));
+                objHashMD5 = null;
+                objDESCrypto.Key = byteHash;
+                objDESCrypto.Mode = CipherMode.ECB; //CBC, CFB
+
+                byteBuff = Convert.FromBase64String(strEncrypted);
+                string strDecrypted = ASCIIEncoding.ASCII.GetString(objDESCrypto.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+                objDESCrypto = null;
+
+                return strDecrypted;
+            }
+            catch (Exception ex)
+            {
+                return "Wrong Input. " + ex.Message;
+            }
+        }
+         private void btnKoduj_Click(object sender, EventArgs e)
+        {
+           txtout.Text=Encrypt(txtin.Text, klucz.Text);
+        }
+
+        private void Szyfrator_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnDekoduj_Click(object sender, EventArgs e)
+        {
+            txtout.Text = Decrypt(txtin.Text, klucz.Text);
         }
     }
 }
